fe89dd2461e24400668e374519c9f932c0ac02ff
fumiko0701 <zx3rainbow@gmail.com>
2025-10-05 20:42:47 -0300

    Comentários uteis no server.js e app.js para ajudar a desenvolver rotas

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
+//usando a biblioteca dotenv do nodejs para carregar variaveis de ambiente
 require('dotenv').config();
-const app = require('./src/app');
+const app = require('./src/app'); //poderia setar aqui as rotas, mas preferi deixar no app.js
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
-});
+  console.log(`Acesse sua perdição em http://localhost:${PORT}`);
+}); //Futuramente poderia adicionar os outros endereços que o servidor poderia rodar
--- src/app.js
+++ src/app.js
@@ -1,15 +1,22 @@
-const express = require('express');
-const app = express();
+const express = require('express'); //importando o express
+const app = express(); //criando a aplicação (instância)
 
-app.use(express.json());
+app.use(express.json()); //habilitando o express para receber json
 
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
   next();
-});
+}); //middleware simples para logar as requisições (pega todas e dá um console.log)
+//eu poderia adicionar cores ou um sistema de logger melhor no futuro
+
 
 app.get('/ping', (req, res) => {
   res.json({ message: 'pong' });
-});
+}); //rota de teste
+// URGENT: JAMAIS ADICIONE OUTRAS ROTAS AQUI.
+// Para adicionar outras rotas, importe um arquivo de rotas
+// Ex:
+// const userRoutes = require('./routes/userRoutes');
+// app.use('/users', userRoutes);
 
 module.exports = app;
