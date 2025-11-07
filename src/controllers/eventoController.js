169c19159d11dc397b7df6403285d3b4f4eefe71
Pedro Izidio <pedrosilvasilva879@gmail.com>
2025-11-07 00:09:04 -0300

    algumas alterações importantes

---
+++ src/controllers/eventoController.js
@@ -0,0 +1,31 @@
+import { pool } from '../config/db.js'
+
+// TODO: APENAS EXEMPLO, NECESSARIO PREENCHER E IMPLEMENTAR
+// listar eventos
+export const listarEventos = async (req, res) => {
+ 
+}
+
+// TODO: APENAS EXEMPLO, NECESSARIO PREENCHER E IMPLEMENTAR
+// criar eventos
+export const criarEvento = async (req, res) => {
+
+}
+
+// TODO: APENAS EXEMPLO, NECESSARIO PREENCHER E IMPLEMENTAR
+// Buscar eventos por ID
+export const buscarEventoPorId = async (req, res) => {
+
+}
+
+// TODO: APENAS EXEMPLO, NECESSARIO PREENCHER E IMPLEMENTAR
+// Atualizar eventos
+export const atualizarEvento = async (req, res) => {
+
+}
+
+// TODO: APENAS EXEMPLO, NECESSARIO PREENCHER E IMPLEMENTAR
+// Deletar eventos
+export const excluirEvento = async (req, res) => {
+
+}
---
+++ src/controllers/localController.js
@@ -0,0 +1,31 @@
+import { pool } from '../config/db.js'
+
+// TODO: APENAS EXEMPLO, NECESSARIO PREENCHER E IMPLEMENTAR
+// listar local
+export const listarLocais = async (req, res) => {
+ 
+}
+
+// TODO: APENAS EXEMPLO, NECESSARIO PREENCHER E IMPLEMENTAR
+// criar local
+export const criarLocal = async (req, res) => {
+
+}
+
+// TODO: APENAS EXEMPLO, NECESSARIO PREENCHER E IMPLEMENTAR
+// Buscar local por ID
+export const buscarLocalPorId = async (req, res) => {
+
+}
+
+// TODO: APENAS EXEMPLO, NECESSARIO PREENCHER E IMPLEMENTAR
+// Atualizar local
+export const atualizarLocal = async (req, res) => {
+
+}
+
+// TODO: APENAS EXEMPLO, NECESSARIO PREENCHER E IMPLEMENTAR
+// Deletar local
+export const excluirLocal = async (req, res) => {
+
+}
